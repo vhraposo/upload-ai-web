c1271dc47e4e4ece3542306978115ab797379309
vhraposo <thevhup@gmail.com>
2023-09-15 14:45:31 -0300

    feat: await api.post - connect with backend

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -6,6 +6,7 @@ import { Textarea } from "./ui/textarea";
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
 import { getFFmpeg } from "@/lib/ffmpeg";
 import { fetchFile } from '@ffmpeg/util'
+import { api } from "@/lib/axios";
 
 export function VideoInputForm(){
     const [videoFile, setVideoFile] = useState<File | null>(null)
@@ -71,7 +72,20 @@ export function VideoInputForm(){
 
         // video to audio
         const audioFile = await convertVideoToAudio(videoFile)
-        console.log(audioFile)
+        
+        const data = new FormData()
+
+        data.append('file', audioFile)
+
+        const response = await api.post('/videos', data)
+
+        const videoId = response.data.video.id
+
+        await api.post(`/videos/${videoId}/transcription`,{
+            prompt
+        })
+
+        console.log('finalizou')
     }
 
     const previewURL = useMemo(() => {
