5d23d86d4af5666d11bf190280537619e164cdfe
vhraposo <thevhup@gmail.com>
2023-09-14 22:40:33 -0300

    feat: add and config ffmpeg lib

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -3,11 +3,11 @@ import { Separator } from "./ui/separator";
 import { FileVideo, Upload } from "lucide-react";
 import { Button } from "./ui/button";
 import { Textarea } from "./ui/textarea";
-import { ChangeEvent, useMemo, useState } from "react";
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
 
 export function VideoInputForm(){
     const [videoFile, setVideoFile] = useState<File | null>(null)
-
+    const promptInputRef = useRef<HTMLTextAreaElement>(null)
     function handleFileSelected(event: ChangeEvent<HTMLInputElement>){
         const { files } = event.currentTarget
 
@@ -16,6 +16,19 @@ export function VideoInputForm(){
         }
         const selectedFile = files[0]
         setVideoFile(selectedFile)
+    }
+
+    function handleUploadVideo(event: FormEvent<HTMLFormElement>){
+        event.preventDefault()
+
+        const prompt = promptInputRef.current?.value
+
+        if(!videoFile){
+            return
+        }
+
+        // video to audio
+        
     }
 
     const previewURL = useMemo(() => {
@@ -27,7 +40,7 @@ export function VideoInputForm(){
     }, [videoFile])
 
     return(
-        <form className='space-y-6'>
+        <form onSubmit={handleUploadVideo} className='space-y-6'>
             <label htmlFor="video"
             className='relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5'
             >
@@ -47,7 +60,7 @@ export function VideoInputForm(){
 
             <div className='space-y-2'>
             <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
-            <Textarea id='transcription_prompt' className='h-20 leading-relaxed resize-none' placeholder='Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)'/>
+            <Textarea ref={promptInputRef} id='transcription_prompt' className='h-20 leading-relaxed resize-none' placeholder='Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)'/>
             </div>
 
             <Button type='submit' className='w-full'>
